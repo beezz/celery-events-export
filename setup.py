4275e799a16762a147b1060a313f72841f7bf8cb
Michal Kuffa <michal.kuffa@gmail.com>
2017-04-05 00:10:24 +0200

    Add install_requires to setup

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 from setuptools import setup
 
+install_requires = [
+    'celery',
+    'elasticsearch',
+]
+
 setup(
     name='celery-events-export',
+    install_requires=install_requires,
     entry_points={
         'celery.commands': [
             'events_export = celery_events_export.cmd:ExportCommand',
