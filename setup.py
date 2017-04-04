dba83d09e2fb7ef932671c829f6e55da3e3e49ec
Michal Kuffa <michal.kuffa@gmail.com>
2017-04-05 01:12:25 +0200

    Add version and update setup.py with author info

--- setup.py
+++ setup.py
@@ -5,12 +5,22 @@ install_requires = [
     'elasticsearch',
 ]
 
+
+def get_version():
+    with open('VERSION') as vf:
+        return vf.read().strip()
+
+
 setup(
     name='celery-events-export',
+    version=get_version(),
     install_requires=install_requires,
+    packages=['celery_events_export'],
     entry_points={
         'celery.commands': [
             'events_export = celery_events_export.cmd:ExportCommand',
         ]
-    }
+    },
+    author='Michal Kuffa',
+    author_email='michal.kuffa@gmail.com',
 )
