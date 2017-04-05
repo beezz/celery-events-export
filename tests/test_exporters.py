481f0d8900cfaa7ff8433be5c434011683e91461
Michal Kuffa <michal.kuffa@gmail.com>
2017-04-05 02:59:39 +0200

    Add test for task_name getter

--- tests/test_exporters.py
+++ tests/test_exporters.py
@@ -1,7 +1,15 @@
-import pytest
 import time
 from datetime import datetime
+
+import pytest
+
 from celery_events_export import exporters
+
+
+class Obj:
+    def __init__(self, **kw):
+        for k, v in kw.items():
+            setattr(self, k, v)
 
 
 class TestExporter:
@@ -21,3 +29,12 @@ class TestExporter:
         ex.add_event_timestamp(event)
         assert ex._add_timestamp in event
         assert isinstance(event[ex._add_timestamp], datetime)
+
+    def test_add_task_name(self):
+        ex = exporters.Exporter({}, {})
+        state = Obj()
+        uuid = 'random-task-uuid'
+        state.tasks = {uuid: Obj(name='task_name')}
+        event = {'uuid': uuid}
+        ex.add_task_name(event, state)
+        assert event['task_name'] == 'task_name'
